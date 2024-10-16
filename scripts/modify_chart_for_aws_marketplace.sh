08524e78ff180bd0b03e5b69db6f3464cee1a5cb
Mateusz Dymiński <dyminski@gmail.com>
2024-10-16 15:25:22 +0200

    Fix AWS marketplace upload
    Signed-off-by: Mateusz Dymiński <dyminski@gmail.com>

--- scripts/modify_chart_for_aws_marketplace.sh
+++ scripts/modify_chart_for_aws_marketplace.sh
@@ -4,10 +4,11 @@ FILE="./stable/rad-plugins/values.yaml"
 
 SOURCE_REGISTRY_NAME="public.ecr.aws/n8h5y2v5/rad-security"
 ECR_PUBLIC_REGISTRY="public.ecr.aws/eks-distro/kubernetes"
+DEPRECATED_ECR_REGISTRY="709825985650.dkr.ecr.us-east-1.amazonaws.com/ksoc-labs"
 
 ECR_REGISTRY_NAME="709825985650.dkr.ecr.us-east-1.amazonaws.com/rad-security"
 
-sed -i "s|$ECR_PUBLIC_REGISTRY|$ECR_REGISTRY_NAME|g" "$FILE"
+sed -i "s|$ECR_PUBLIC_REGISTRY|$DEPRECATED_ECR_REGISTRY|g" "$FILE"
 sed -i "s|$SOURCE_REGISTRY_NAME|$ECR_REGISTRY_NAME|g" "$FILE"
 sed -i '/# --/d' "$FILE"
 
